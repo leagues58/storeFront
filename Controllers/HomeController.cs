035d66697d8c5d762580e1be7c8932fe451ae15d
Eric <leagues58@gmail.com>
2019-04-24 22:32:18 -0400

    Added new index controller and view

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StoreFront.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace StoreFront.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        public HomeController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Product Overview";
+
+            var products = productRepository.GetAllProducts().OrderBy(p => p.Name);
+
+            return View(products);
+        }
+    }
+}
