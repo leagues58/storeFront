278673a77eab3663b22cf969a4666a9794bafaf6
Eric <leagues58@gmail.com>
2019-04-26 00:33:30 -0400

    Added EF core to project, implemented ProductRepository

---
+++ Models/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace StoreFront.Models
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly AppDbContext appDbContext;
+
+        public ProductRepository(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return this.appDbContext.Products;
+        }
+
+        public Product GetProductById(int productId)
+        {
+            return this.appDbContext.Products.FirstOrDefault(p => p.Id == productId);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using StoreFront.Models;
 
@@ -8,11 +10,18 @@ namespace StoreFront
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IProductRepository, MockProductRepository>();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddTransient<IProductRepository, ProductRepository>();
             services.AddMvc();
         }
 
